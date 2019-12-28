094a0311fd2cf9b589a8abba96de8486a557c554
SpencerMilner <milnerspencer@gmail.com>
2019-12-28 11:20:46 -0800

    special case a

--- test/specialCaseA.test.js
+++ test/specialCaseA.test.js
@@ -1,24 +1,50 @@
 const { rules, buildRegex } = require('../src/rules');
 const regex = buildRegex([rules.specialCaseA]);
 
-test('Permit A followed by I or Y', () => {
-  const validTestCases = [
-    "ai",
-    "ay",
-    "aig"
-  ];
-
-  const invalidTestCases = [
-    "gi",
-    "g",
-    "aio"
-  ];
-
-  validTestCases.forEach((testCase) => {
-    expect(regex.test(testCase)).toBe(true);
+describe('Special Case A', () => {
+  test('Match A followed by I or Y', () => {
+    const testCases = [
+      "ai",
+      "ay",
+      "aig"
+    ];
+  
+    testCases.forEach((testCase) => {
+      expect(regex.test(testCase)).toBe(true);
+    });
   });
+  test('Do not match other cases', () => {
+    const testCases = [
+      "gi",
+      "g",
+      "ao",
+      "ag"
+    ];
 
-  invalidTestCases.forEach((testCase) => {
-    expect(regex.test(testCase)).toBe(false);
+    testCases.forEach((testCase) => {
+      expect(regex.test(testCase)).toBe(false);
+    });
+  
+  });
+  describe('The first letter in the next combination must match the alternating vowel/consonant rule', () => {  
+    test('Match vowel/consonant rule', () => {
+      const testCases = [
+        "aig"
+      ];
+
+      testCases.forEach((testCase) => {
+        expect(regex.test(testCase)).toBe(true);
+      });
+    });
+
+    test('Does not match vowel consonant rule', () => {
+      const testCases = [
+        "aio"
+      ];
+
+      testCases.forEach((testCase) => {
+        expect(regex.test(testCase)).toBe(false);
+      });
+    });
   });
 });
