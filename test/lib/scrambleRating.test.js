f749d4943d702eb19bed40d78977aeb6bd4079f0
SpencerMilner <milnerspencer@outlook.com>
2019-12-30 11:17:26 -0800

    More rating tests (#4)
    * more test cases
    
    * update file test case

--- test/lib/scrambleRating.test.js
+++ test/lib/scrambleRating.test.js
@@ -2,23 +2,71 @@ const { getRating } = require('../../src/lib/scrambleRating');
 
 describe('Scramble Rating', () => {
   test('Not a scramble', () => {
-    let scramble = "irony";
-    let word = "irony";
-    expect(getRating(scramble, word)).toBe('not');
+    const scramble = [
+      "irony",
+      "cow",
+      "map",
+    ];
+
+    const word = [
+      "irony",
+      "cow",
+      "map",
+    ];
+
+    for(let i = 0; i < scramble.length; i++){
+      expect(getRating(scramble[i], word[i])).toBe('not');
+    }
   });
   test('Fair scramble', () => {
-    const scramble = "maps";
-    const word = "spam";
-    expect(getRating(scramble, word)).toBe('fair');
+    const scramble = [
+      "maps",
+      "riony",
+      "INOYR",
+    ];
+
+    const word = [
+      "spam",
+      "IRONY",
+      "irony",
+    ];
+
+    for(let i = 0; i < scramble.length; i++){
+      expect(getRating(scramble[i], word[i])).toBe('fair');
+    }
   });
   test('Poor scramble', () => {
-    const scramble = "ioyrn";
-    const word = "irony";
-    expect(getRating(scramble, word)).toBe('poor');
+    const scramble = [
+      "ioyrn",
+      "pcak",
+      "trewched",
+    ];
+
+    const word = [
+      "irony",
+      "pack",
+      "wretched",
+    ];
+
+    for(let i = 0; i < scramble.length; i++){
+      expect(getRating(scramble[i], word[i])).toBe('poor');
+    }
   });
   test('Hard scramble', () => {
-    const scramble = "onyri";
-    const word = "irony";
-    expect(getRating(scramble, word)).toBe('hard');
+    const scramble = [
+      "onyri",
+      "wrope",
+      "rabaclen"
+    ];
+
+    const word = [
+      "irony",
+      "power",
+      "barnacle"
+    ];
+
+    for(let i = 0; i < scramble.length; i++){
+      expect(getRating(scramble[i], word[i])).toBe('hard');
+    }
   });
 });
